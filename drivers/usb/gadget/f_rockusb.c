356e892df7cc9e039719e6b33052fb2ffa94115b
tzuwen_chang <TzuWen_Chang@asus.com>
2020-11-10 01:28:39 +0000

    defconfig: Modify the UMS and bootloader VID/PID to ASUS.
    Change-Id: I4e1bd716dfaf2050b04f821761ff3e3a899be6a3
    Signed-off-by: Rock Shen <rock_shen@asus.com>

--- drivers/usb/gadget/f_rockusb.c
+++ drivers/usb/gadget/f_rockusb.c
@@ -86,8 +86,8 @@ int g_dnl_bind_fixup(struct usb_device_descriptor *dev, const char *name)
 #endif
 	} else if (!strncmp(name, "usb_dnl_fastboot", 16)) {
 		/* Fix to Google's VID and PID */
-		dev->idVendor  = __constant_cpu_to_le16(0x18d1);
-		dev->idProduct = __constant_cpu_to_le16(0xd00d);
+		dev->idVendor  = __constant_cpu_to_le16(0x0b05);
+		dev->idProduct = __constant_cpu_to_le16(0x7771);
 	} else if (!strncmp(name, "usb_dnl_dfu", 11)) {
 		/* Fix to Rockchip's VID and PID for DFU */
 		dev->idVendor  = cpu_to_le16(0x2207);
