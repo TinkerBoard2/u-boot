c00c29df90de67f18b4d24f9183a9fc0dfdb0bd6
frank_chiang <frank_chiang@asus.com>
2020-12-21 17:43:06 +0800

    Set GPIO2_A4 (MASK_RELEASE) output high.
    Change-Id: Ibd26a43990843cdfe8c0d59388c8b4c22e496210

--- arch/arm/mach-rockchip/boot_rkimg.c
+++ arch/arm/mach-rockchip/boot_rkimg.c
@@ -34,7 +34,24 @@
 #include <u-boot/sha256.h>
 #include <linux/usb/phy-rockchip-inno-usb2.h>
 
+#define GPIO2_SWPORTA_DDR_REG	0xff780004
+#define GPIO2_SWPORTA_DR_REG	0xff780000
+
 DECLARE_GLOBAL_DATA_PTR;
+
+void rk3399_mask_release_high(void)
+{
+	printf("Set GPIO2_A4 (MASK_RELEASE) output high\n");
+
+	uint32_t reg_gpio2a_ddr = readl((void *)GPIO2_SWPORTA_DDR_REG);
+	uint32_t reg_gpio2a_dr = readl((void *)GPIO2_SWPORTA_DR_REG);
+
+	// Set GPIO2_A4 to direction output.
+	writel(reg_gpio2a_ddr | (1 << 4), GPIO2_SWPORTA_DDR_REG);
+
+	// Set GPIO2_A4 to High.
+	writel(reg_gpio2a_dr | (1 << 4), GPIO2_SWPORTA_DR_REG);
+}
 
 static void boot_devtype_init(void)
 {
@@ -46,6 +63,8 @@ static void boot_devtype_init(void)
 
 	if (done)
 		return;
+
+	rk3399_mask_release_high();
 
 	ret = param_parse_bootdev(&devtype, &devnum);
 	if (!ret) {
